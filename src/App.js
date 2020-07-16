f2fe3881c56585e76e65f3dbfdbf05b8f1684d35
stewart <npcuong.011308@gmail.com>
2020-07-16 13:24:46 +0700

    Lifting state up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import TodoList from './components/todo_list'
 
-function App() {
-  return (
-    <TodoList />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          content: "Test",
+          completed: false
+        },
+        {
+          id: 2,
+          content: "Test 2",
+          completed: true
+        }
+      ]
+    }
+  }
+
+  render() {
+    return(
+      <TodoList todos={this.state.todos}/>
+    )
+  }
 }
 
 export default App;
--- src/components/todo_list/index.js
+++ src/components/todo_list/index.js
@@ -2,25 +2,17 @@ import React from "react"
 import TodoItem from "../todo_item"
 
 class TodoList extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      todos: [
-        {
-          id: 1,
-          content: "Test",
-          completed: false
-        }
-      ]
-    }
-  }
-
   render() {
-    return(
-      <div>
-        {this.state.todos.map(todo => <TodoItem id={todo.id} content={todo.content} completed={todo.completed}/>)}
-      </div>
+    const todos = this.props.todos
+
+    return (
+      <ul>
+        {todos.map(todo =>
+          <li>
+            <TodoItem id={todo.id} content={todo.content} completed={todo.completed} />
+          </li>
+        )}
+      </ul>
     )
   }
 }
