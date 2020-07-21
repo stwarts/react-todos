4f122756d5533666e91c054db2b8610987742b10
stewart <npcuong.011308@gmail.com>
2020-07-21 19:04:25 +0700

    Use localStoreage

--- src/App.js
+++ src/App.js
@@ -2,32 +2,30 @@ import React from "react";
 import TodoList from "./components/todo_list"
 import TodoForm from "./components/todo_form"
 
+const setTodos = (todos) => {
+  localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+
+const getTodos = () => {
+  return JSON.parse(localStorage.getItem('todos')) || []
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      todos: [
-        {
-          id: 1,
-          content: "Test",
-          completed: false
-        },
-        {
-          id: 2,
-          content: "Test 2",
-          completed: true
-        }
-      ]
+      todos: getTodos()
     }
   }
 
   createTodo = (newTodo) => {
-    const oldTodos = this.state.todos
+    const todos = [...this.state.todos, newTodo]
 
-    this.setState({
-      todos: [...oldTodos, newTodo]
-    })
+    setTodos(todos)
+
+    this.setState({ todos: todos })
   }
 
   toggleStatus = (todoId) => {
@@ -35,32 +33,32 @@ class App extends React.Component {
       return todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
     })
 
-    this.setState({
-      todos: todos
-    })
+    setTodos(todos)
+
+    this.setState({ todos: todos })
   }
 
   deleteTodo = (todoId) => {
     const todos = this.state.todos
     const newTodos = todos.filter(todo => todo.id !== todoId)
 
-    this.setState({
-      todos: newTodos
-    })
+    setTodos(newTodos)
+
+    this.setState({ todos: newTodos })
   }
 
   editTodo = (todoId, newContent) => {
     const todos = this.state.todos
-    
+
     let editedTodo
 
-    const newTodos = todos.map(todo => { 
+    const newTodos = todos.map(todo => {
       return todo.id === todoId ? editedTodo = { ...todo, content: newContent } : todo
     })
 
-    this.setState({
-      todos: newTodos
-    })
+    setTodos(newTodos)
+
+    this.setState({ todos: newTodos })
 
     return editedTodo
   }
